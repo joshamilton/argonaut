59403906d2413f77f1b64aebe76254aea0edfc76
emilytrybulec <114685119+emilytrybulec@users.noreply.github.com>
2024-01-29 14:12:40 -0500

    Update NfcoreTemplate.groovy

--- lib/NfcoreTemplate.groovy
+++ lib/NfcoreTemplate.groovy
@@ -322,15 +322,9 @@ class NfcoreTemplate {
         String workflow_version = NfcoreTemplate.version(workflow)
         String.format(
             """\n
-            ${dashedLine(monochrome_logs)}
-                                                    ${colors.green},--.${colors.black}/${colors.green},-.${colors.reset}
-            ${colors.blue}     /\     |⁻⁻⁻⁻|  |⁻⁻⁻⁻⁻   |⁻⁻⁻⁻⁻|	|⁻\    |    /\	   | 	  | ⁻⁻⁻⁻|⁻⁻⁻⁻     ${colors.green}/,-._.--~\'${colors.reset}
-            ${colors.blue}    /_\    |₋₋₋₋|  |	     |	     |	|  \   |   /_\	   | 	  |	 |         ${colors.yellow}}  {${colors.reset}
-            ${colors.blue}   /    \   |    \   |  	⁻⁻|  |	     |	|   \  |  / 	\  |	  | 	 |	     ${colors.green}\\`-._,-`-,${colors.reset}
-                                                    ${colors.green}`._,._,\'${colors.reset}
-            ${colors.blue}   /      \  |     \  |___|  |___|	|    \₋| /  	 \ |___|	 |  ${colors.reset}
-            ${colors.purple}  ${workflow.manifest.name} ${workflow_version}${colors.reset}
-            ${dashedLine(monochrome_logs)}
+            ${colors.blue}        ___     __   __   __   ___     ${colors.green}/,-._.--~\'${colors.reset}
+            ${colors.blue}  |\\ | |__  __ /  ` /  \\ |__) |__         ${colors.yellow}}  {${colors.reset}
+            ${colors.blue}  | \\| |       \\__, \\__/ |  \\ |___     ${colors.green}\\`-._,-`-,${colors.reset}
             """.stripIndent()
         )
     }
